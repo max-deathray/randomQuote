388e4f5f7b2b688eb0b0395a9351d4a3172c39b4
McRae Petrey <mcrae@McRaes-MacBook-Pro-2018.local>
2019-01-06 23:05:26 -0500

    hooked up the randome quote generator

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5018ffbc5b62bff88302";
+/******/ 	var hotCurrentHash = "49d039d3db6c7e97b065";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,58 @@
 import Layout from '../components/MyLayout.js';
-import Link from 'next/link';
+import React from 'react';
+import db from '../db';
 
-const PostLink = props => (
-  <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
-);
+class QuoteBox extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      current: {
+        quote:
+          "If you pour some music on whatever's wrong, it'll sure help out.",
+        author: 'Levon Helm',
+      },
+    };
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('component did mount!');
+  }
+
+  clickHandler() {
+    console.log('clicked!');
+    this.randomize(db.quotes);
+    // console.log(this.state);
+  }
+
+  randomize(arr) {
+    let index = Math.ceil(Math.random() * arr.length);
+    console.log('index', index);
+    let current = arr[index];
+    this.setState({
+      current: current,
+    });
+    console.log('new state is ', this.state);
+  }
+
+  render() {
+    return (
+      <div id="quote-box">
+        <div id="text">
+          <p>{this.state.current.quote}</p>
+        </div>
+        <div id="author">{this.state.current.author}</div>
+        <button id="new-quote" onClick={this.clickHandler}>
+          New Quote
+        </button>
+      </div>
+    );
+  }
+}
 
 export default () => (
   <Layout>
     <h1>thank u, next.js</h1>
-    <ul>
-      <PostLink id="hello-nextjs" title="Hello Next.js" />
-      <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
-      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
-    </ul>
+    <QuoteBox />
   </Layout>
 );
